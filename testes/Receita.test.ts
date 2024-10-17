9b2325dc4f274809dad910547ae74da137e80f7f
Gabriel Martins <126828493+gbrielmartinssreo@users.noreply.github.com>
2024-10-17 15:17:30 -0400

    Update Receita.test.ts

--- testes/Receita.test.ts
+++ testes/Receita.test.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Receita } from "../classes/Receita";
 import { Consulta } from "../classes/Consulta";
 import { Medico } from "../classes/Medico";
@@ -49,5 +48,4 @@ describe('receita', () =>{
     
     
 })
-=======
->>>>>>> 4edc3e7d568d41568cf5abe0b95ed164bc234d2a
+
